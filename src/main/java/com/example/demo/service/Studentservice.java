1bfb0d582f5dc7b327ca10df946ef596c529b707
bala-amypo <bala@amypo.in>
2025-12-18 04:49:14 +0000

    User commit

--- src/main/java/com/example/demo/service/Studentservice.java
+++ src/main/java/com/example/demo/service/Studentservice.java
@@ -8,6 +8,6 @@ public interface  Studentservice{
        Studententity postData(Studententity stu); // method declaration and stu is the users giving detail
 
        List<Studententity>getAllData();                 //method declaration for rereiving all the data
-
        
+
 }
